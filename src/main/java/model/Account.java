61c8c908dc65242e5416ec177b78bf24ef791373
danielnorberg1 <daniel.norberg64@gmail.com>
2023-11-20 12:03:05 +0100

    Created constructor for Customer class

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -2,4 +2,5 @@ package src.main.java.model;
 
 public class Account {
 
+
 }
--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,4 +1,34 @@
 package src.main.java.model;
 
+import java.util.HashMap;
+
 public class Customer {
+
+    private HashMap<Account, Integer> accounts;
+   private String firstName;
+   private String lastName;
+   final int PERSONAL_NUMBER;
+
+
+   public Customer(String firstname, String lastName, int PERSONAL_NUMBER){
+       this.PERSONAL_NUMBER = PERSONAL_NUMBER;
+       this.firstName = firstname;
+       this.lastName = lastName;
+       this.accounts = new HashMap<Account, Integer>();
+
+   }
+
+   public String veiwAllAccounts(){
+
+   }
+
+   public String veiwAccount(){
+
+   }
+
+
+
+
+
+
 }
