c509bc6c3f76c1932cfb7b7d541ff2677b9e904c
Martin Lidgren <martin.lidgren@live.se>
2023-11-20 12:14:06 +0100

    Made constructor for Account class

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,6 +1,27 @@
 package src.main.java.model;
 
+import java.util.HashMap;
+
 public class Account {
 
+<<<<<<< HEAD
+    private HashMap<Transaction, Integer> transactions;
+    private double balance;
+    private final int accountID;
+
+
+    public Account(int accountID){
+        this.transactions = new HashMap<Transaction, Integer>();
+        this.balance = 0;
+        this.accountID = accountID;
+    }
+
+    public Transaction sendTransaction(Account toAccount){
+        return new Transaction();
+    }
+
+    public Transaction receiveTransaction(Account fromAccount){
+        return new Transaction();
+    }
 
 }
