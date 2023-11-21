70c18c239baf11bb5f99ba9ca033e1acdeba6ef5
eddedomer <31513157+eddedomer@users.noreply.github.com>
2023-11-21 14:22:00 +0100

    Changes to createAccount and exceptions

--- src/main/java/model/Valvet.java
+++ src/main/java/model/Valvet.java
@@ -12,15 +12,21 @@ public class Valvet {
 
     public String createCustomer(String firstName, String lastName, int personalNumber) throws Exception{
         if (customers.containsKey(personalNumber)){
-            throw new CustomerAlreadyExistsException("Customer already exists.");
+            throw new AlreadyExistsException("Customer already exists.");
         }
         Customer newCustomer = new Customer(firstName, lastName, personalNumber);
         customers.put(personalNumber, newCustomer);
         return "Customer: " + newCustomer + " successfully created!";
     }
 
-    public String createAccount(){
-
+    public String createAccount(int accountID, int personalNumber) throws Exception{
+        for (Customer customer : customers.values()) {
+                if (customer.getAccounts().get(accountID) != null){
+                    throw new AlreadyExistsException(accountID);
+                }
+            }
+        Customer customer = customers.get(personalNumber);
+        customer.createAccount(accountID)
     }
 
     public String deleteCustomer(){
