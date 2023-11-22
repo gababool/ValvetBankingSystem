391b78fc9ab5b51e56acf1d80164bb7cf2cc6499
eddedomer <31513157+eddedomer@users.noreply.github.com>
2023-11-22 14:32:56 +0100

    Changes to the code by martin

--- src/main/java/model/Valvet.java
+++ src/main/java/model/Valvet.java
@@ -45,7 +45,7 @@ public class Valvet implements Serializable {
         return "Account " + accountID + "was successfully created.";
     }
 
-    public void updatePersonalInformation(){
+    public void updateCustomerFirstName(){
 
     }
 
