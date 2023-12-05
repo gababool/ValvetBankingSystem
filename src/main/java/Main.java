697759a9ab10c6bf1036a767c932353f0dc9e548
Martin Lidgren <martin.lidgren@live.se>
2023-12-05 15:22:11 +0100

    Changed the fxml files to have access to the controllers. Changed main the be ready for SceneBuilder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,42 @@
 package src.main.java;
 
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import src.main.java.model.Valvet;
 import src.main.java.model.ValvetFileManager;
-import src.main.java.model.Customer;
+import javafx.application.Application;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) throws Exception {
         // 1. Load Valvet, customers, accounts and transactions from data.json
         // Create a bank with customers, accounts, and transactions
         Valvet valv = new Valvet("1337");
+
         // Populate your bank with data...
-       
+        // MAIN MENU HERE
 
-        valv.createAccount(11111);
-
-        System.out.println(valv.getCustomer(11111).viewAllAccounts());
-        System.out.println(valv.getCustomer(22222).viewAllAccounts());
-
-
-
-        valv.viewAllCustomers();
         // Save the bank data to a JSON file
         ValvetFileManager.saveBank(valv);
 
         // Load the bank data from the JSON file
         Valvet loadedBank = ValvetFileManager.loadBank();
 
-        loadedBank.viewAllCustomers();
-        System.out.println(valv.getCustomer(11111).viewAllAccounts());
-        System.out.println(valv.getCustomer(22222).viewAllAccounts());
-
         // Display the loaded bank data
         if (loadedBank != null) {
             System.out.println(loadedBank);
 
         }
-
-
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
 
